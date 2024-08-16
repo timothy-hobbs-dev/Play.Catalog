0121c42f7db99a61ebb341c5afeb871b80a76bd1
timothy-kumah <timothy.kumah@amalitech.com>
2024-08-16 09:39:01 +0000

    integrated controller with repository

--- Play.Catalog.Service/Repositories/ItemsRepository.cs
+++ Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -23,12 +23,23 @@ public class ItemsRepository
         return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
     }
 
+    /// <summary>
+    /// Retrieves an item from the database by its ID.
+    /// </summary>
+    /// <param name="id">The ID of the item to be retrieved.</param>
+    /// <returns>A task that represents the asynchronous operation, containing the item if found, or null otherwise.</returns>
     public async Task<Item?> GetAsync(Guid id)
     {
         var filter = filterBuilder.Eq(item => item.Id, id);
         return await dbCollection.Find(filter).FirstOrDefaultAsync();
     }
 
+    /// <summary>
+    /// Creates a new item in the database.
+    /// </summary>
+    /// <param name="entity">The item to be created.</param>
+    /// <exception cref="ArgumentNullException">Thrown if the entity is null.</exception>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task CreateAsync(Item entity)
     {
         if (entity is null)
@@ -53,7 +64,11 @@ public class ItemsRepository
         var filter = filterBuilder.Eq(item => item.Id, entity.Id);
         await dbCollection.ReplaceOneAsync(filter, entity);
     }
-
+    /// <summary>
+    /// Removes an item from the database.
+    /// </summary>
+    /// <param name="id">The ID of the item to be removed.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task RemoveAsync(Guid id)
     {
         var filter = filterBuilder.Eq(item => item.Id, id);
